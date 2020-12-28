7fee08150f3ce7fcb9d8abbfd2e685c44438f71a
楠瀬光 <kusunosehikari@kusunosehikarinoMacBook-Pro.local>
2020-12-28 21:51:42 +0900

    definition.jsファイルの修正
    変数名の修正

--- vue/src/static/definition.js
+++ vue/src/static/definition.js
@@ -3,9 +3,9 @@ const months = [];
 const dates = [];
 
         for(let y = 1967; y < 2000; y++) {
-                const set_data = new Date(y, 0, 0);
+                const data = new Date(y, 0, 0);
                 const opt = {year:'numeric'};
-                const wareki = set_data.toLocaleDateString("ja-JP-u-ca-japanese", opt);
+                const wareki = data.toLocaleDateString("ja-JP-u-ca-japanese", opt);
                 years.push(`${y}年　（${wareki}）`)
         }
 
